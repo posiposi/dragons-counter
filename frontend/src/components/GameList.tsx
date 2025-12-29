71b09a0cc1c8fc1877b205eb4b42d565b8bd3c68
Daichi Sugiyama <caad10.shimano105@gmail.com>
2025-12-29 21:14:14 +0900

    lint修正

--- frontend/src/components/GameList.tsx
+++ frontend/src/components/GameList.tsx
@@ -49,7 +49,7 @@ export default function GameList() {
       await deleteGame(deleteTargetGame.id);
 
       setGames((prevGames) =>
-        prevGames.filter((game) => game.id !== deleteTargetGame.id)
+        prevGames.filter((game) => game.id !== deleteTargetGame.id),
       );
 
       setDeleteResult({
